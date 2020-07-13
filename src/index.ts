ea4048c64c80ee0596f84ad0ab68d686019a7a2b
cpustejovsky <charles.pustejovsky@gmail.com>
2020-07-13 16:41:45 -0400

    setting up User class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Howdy, partner!")
+import { User } from "./models/User";
+
+const user = new User({ name: "Charles", age: 27 });
+
+user.set({ name: "Charles Clinton Pustejovsky III" });
+
+user.on("change", () => {
+  console.log("test");
+});
+
+console.log(user.get("name"));
+console.log(user.get("age"));
+
+user.trigger("change");
+
